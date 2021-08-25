4c03138079df4a3ef0ea0f89fa4c6f47c1dbf6a3
wenqiang.zhang <wenqiang.zhang@haiziwang.com>
2021-08-25 16:20:02 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 import { routes } from "@/routes";
+import RouterView from "@com/RouterView";
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={null}>
         <Switch>
-          <Route exact path="/" render={() => <Redirect to="/index" />} />
-          {routes.map(({ component: Component, path }) => (
-            <Route
-              key={path}
-              path={path}
-              exact
-              render={(props) => <Component {...props} />}
-            />
-          ))}
+          <RouterView routes={routes} />
         </Switch>
       </Suspense>
     </BrowserRouter>
